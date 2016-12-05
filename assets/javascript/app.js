981385b43c9592c4ca82d110d3505a6f307f0684
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-04 21:57:56 -0500

    setting up background and layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+})
+        // // If the user completed the word, tell them they won, then reset everything  
+        // if (comp_checker()) {
+        //   reset_hint();
+        //   setTimeout(win_display, 100)
+        //   setTimeout(reset, 2900)
+        
+        // // If the user correctly guessed a letter but still has more work to do, just update the skeleton 
+        // }else if (correct_guess) {
+        //   $("#skeldiv").html(skeleton);
+
+        // // If the user incorrectly guessed and it was their final guess, tell them they lost, reveal correct answer, then reset everything. 
+        // }else if (guesses_remaining == 1) {      
+        //   reset_hint();
+        //   setTimeout(lose_display, 100)
+        //   setTimeout(reset, 3200)         
